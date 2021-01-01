0177d867fecc6ef3956914f4a953b08fa3e53df5
Antoo <35061001+AntoTGBFR@users.noreply.github.com>
2021-01-01 16:20:05 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Hello World In Java by Antoo
+// 01 Jan 2021
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printIn("Hello World!");
+    }
+}
---
+++ HelloWorld.js
@@ -0,0 +1,6 @@
+// Hello World in JavaScript by Antoo
+// 01 Jan 2021
+
+//Hello World using console.log()
+
+console.log("Hello World!");
